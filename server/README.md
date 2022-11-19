6105d6f4611bd923ecce3652bd6c3113506d4c24
Krystyna Waniova <krystyna.waniova@gmail.com>
2022-11-19 21:54:31 +0100

    exp

---
+++ server/README.md
@@ -0,0 +1,61 @@
+Source: https://strawberry.rocks/docs
+
+// to launch
+python -m venv virtualenv
+source virtualenv/bin/activate
+pip install 'strawberry-graphql[debug-server]'
+
+strawberry server schema
+
+### exp_1
+
+```
+query {
+ books {
+    title
+    author
+  }
+}
+```
+
+### exp_2
+
+```
+query {
+ books {
+    title
+    author {
+      name
+    }
+  }
+
+  authors {
+    name
+  }
+}
+```
+
+### exp_3
+
+```
+{
+  books {
+    title
+    id
+  }
+  authors {
+    name
+    id
+  }
+}
+```
+
+### exp_5
+
+```
+query{
+  greet
+  # greet(name: null)
+  # greet(name: "Alfons")
+}
+```
---
+++ server/schema_exp_3.py
@@ -0,0 +1,74 @@
+import typing
+import strawberry
+
+books = [
+    {"id": 1, "title": "Harry Potter", "author_id": 1},
+    {"id": 2, "title": "Lord of The Rings", "author_id": 2},
+    {"id": 3, "title": "1984", "author_id": 3}
+]
+
+authors = [
+    {"id": 1, "name": 'J.K.Rowling'},
+    {"id": 2, "name": 'J. R. R. Tolkien'},
+    {"id": 3, "name": 'G. Orwell'},
+]
+
+
+def get_author_for_book(root) -> "Author":
+    print("ROOT: ", root)
+    return Author(name="Michael Crichton")
+
+
+@strawberry.type(description="This is book class")
+class Book:
+    id: strawberry.ID
+    title: str
+    author: "Author" = strawberry.field(resolver=get_author_for_book)
+
+
+def get_books_for_author(root):
+    return [Book(id=book['id'], title=book["title"]) for book in filter(lambda x: x.get("author_id") == root.id, books)]
+
+
+@strawberry.type
+class Author:
+    id: strawberry.ID
+    name: str
+    books: typing.List[Book] = strawberry.field(resolver=get_books_for_author)
+
+
+def get_authors() -> typing.List[Author]:
+    return [Author(id=author['id'], name=author["name"]) for author in authors]
+
+
+def get_books() -> typing.List[Author]:
+    return [Book(id=book['id'], title=book["title"]) for book in books]
+
+
+@strawberry.type
+class Query:
+    authors: typing.List[Author] = strawberry.field(resolver=get_authors)
+    books: typing.List[Book] = strawberry.field(resolver=get_books)
+
+
+@strawberry.input
+class AddBookForAuthor:
+    title: str
+    author_id: strawberry.ID
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_book(self, inputBook: AddBookForAuthor) -> None:
+        author = list(filter(lambda x: x.id == int(
+            inputBook.author_id), get_authors()))[0]
+        books.append(
+            {"id": len(books), "title": inputBook.title, "author_id": author.id})
+
+    @strawberry.mutation  # nic nie zwraca
+    def restart() -> None:
+        print(f'Restarting the server')
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
